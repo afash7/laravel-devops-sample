22ecc80fcede5e2b2f9f005a967df5e6f320aead
Afash <29870088+afash7@users.noreply.github.com>
2026-02-05 20:34:21 +0330

    k6 debug

--- load-test/k6.js
+++ load-test/k6.js
@@ -1,17 +1,13 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
 
-export let options = {
-  vus: 50,          // Virtual Users
-  duration: '30s',  // Test duration
+export const options = {
+  vus: 50,
+  duration: '30s',
 };
 
 export default function () {
-
-  // ===============================
-  // Fetch API
-  // ===============================
-  let res = http.get('http://localhost/api/health');
+  const res = http.get('http://nginx/api/health');
 
   check(res, {
     'status is 200': (r) => r.status === 200,
